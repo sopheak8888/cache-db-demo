fb38c2736649545d05a1977f8c59e3b7d4522147
John <limsopheak89@gmail.com>
2023-11-01 20:53:48 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder()
+    .setTitle('DB Cache Demo')
+    .setDescription('This is a automate create docker container api')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      defaultModelsExpandDepth: -1,
+      persistAuthorization: true,
+    },
+  });
+  await app.listen(3000).then(() => {
+    console.log('Server is running on http://localhost:3000');
+  });
+}
+bootstrap();
