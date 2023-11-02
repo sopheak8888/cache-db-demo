313ba0e728f75eb41dbd373a0a86fafcc617b686
John <limsopheak89@gmail.com>
2023-11-02 14:06:55 +0700

    feat: apply redis cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { BookModule } from './book/book.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import type { RedisClientOptions } from 'redis';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -17,6 +20,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      cache: {
+        type: process.env.CACHE_TYPE as any,
+        options: {
+          host: process.env.REDIS_HOST,
+          port: parseInt(process.env.REDIS_PORT as any),
+        },
+        ignoreErrors: true,
+      },
+    }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      type: process.env.CACHE_TYPE as any,
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT as any),
+      isGlobal: true,
     }),
     BookModule,
   ],
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,43 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Book } from './entities/book.entity';
 import { FindAllBookDto } from './dto/find-all-book.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class BookService {
   constructor(
     @InjectRepository(Book)
     private bookRepository: Repository<Book>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async create(createBookDto: CreateBookDto) {
     const result = await this.bookRepository.save(createBookDto);
+    await this.cacheManager.del('book_list_*');
     return result;
   }
 
   async findAll(filter: FindAllBookDto) {
+    const cache = await this.cacheManager.get(
+      'book_list_' + JSON.stringify(filter),
+    );
+    if (cache) return cache;
     const result = await this.bookRepository.findAndCount({
       skip: (filter.page - 1) * filter.limit,
       take: filter.limit,
     });
+    await this.cacheManager.set('book_' + JSON.stringify(filter), result);
     return result;
   }
 
   async findOne(id: number) {
+    const cache = await this.cacheManager.get('book_' + id);
+    if (cache) return cache;
     const result = await this.bookRepository.findOneBy({ id });
+    if (result) await this.cacheManager.set('book_' + id, result);
     return result;
   }
 
   async update(id: number, updateBookDto: UpdateBookDto) {
     const result = await this.bookRepository.update(id, updateBookDto);
+    if (result.affected > 0) {
+      await this.cacheManager.del('book_list_*');
+      await this.cacheManager.del('book_' + id);
+    }
     return result;
   }
 
   async remove(id: number) {
     const result = await this.bookRepository.delete(id);
+    if (result.affected > 0) {
+      await this.cacheManager.del('book_list_*');
+      await this.cacheManager.del('book_' + id);
+    }
     return result;
   }
 }
